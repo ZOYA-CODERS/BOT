9d15a8995e0094aafe24b208d62e9c97fe120ffd
ZOYA-CODERS <loversmusic0019@gmail.com>
2025-07-26 18:13:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const socketio = require("socket.io");
+const bodyParser = require("body-parser");
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.static('public'));
+mongoose.connect("mongodb+srv://Zoya_coderz:@Passed123#@cluster0.lodgdul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true });
 
-io.on('connection', socket => {
-  console.log('A user connected');
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+const Message = mongoose.model("Message", new mongoose.Schema({
+  username: String,
+  message: String,
+  time: { type: Date, default: Date.now }
+}));
+
+const adminPassword = "@Passed123#"; // change this!
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.post("/admin", (req, res) => {
+  if (req.body.password === adminPassword) {
+    res.redirect("/");
+  } else {
+    res.send("Wrong password");
+  }
+});
+
+io.on("connection", async (socket) => {
+  const lastMessages = await Message.find().sort({ time: -1 }).limit(75);
+  socket.emit("loadMessages", lastMessages.reverse());
+
+  socket.on("sendMessage", async (data) => {
+    const msg = new Message(data);
+    await msg.save();
+    io.emit("newMessage", data);
   });
 });
 
-http.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+server.listen(3000, () => console.log("Chat app running on port 3000"));
