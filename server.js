28a5dd55d59e32ced50e5305c8b1bb8fb8fd71b6
ZOYA-CODERS <loversmusic0019@gmail.com>
2025-07-27 06:55:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://Zoya_coderz:@Passed123#@cluster0.lodgdul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  time: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
 
 app.use(express.static('public'));
 
-io.on('connection', socket => {
-  console.log('A user connected');
-  socket.on('chat message', msg => {
+io.on('connection', async (socket) => {
+  const lastMessages = await Message.find().sort({ time: -1 }).limit(25);
+  socket.emit('loadMessages', lastMessages.reverse());
+
+  socket.on('chat message', async (msg) => {
+    const newMsg = new Message({ text: msg });
+    await newMsg.save();
     io.emit('chat message', msg);
-  });
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
   });
 });
 
-http.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
