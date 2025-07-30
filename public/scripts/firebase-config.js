02b471fd4eeae6cdd15f726ec26778e0489a49a5
ZOYA-CODERS <loversmusic0019@gmail.com>
2025-07-30 17:26:59 +0530

    Create firebase-config.js

---
+++ public/scripts/firebase-config.js
@@ -0,0 +1,17 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = firebase.auth();
+const database = firebase.database();
