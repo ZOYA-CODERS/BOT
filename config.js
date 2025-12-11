a02ddd370677a3951360d9e289f3fcc82ade077e
ZOYA-CODERS <loversmusic0019@gmail.com>
2025-12-11 08:24:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,17 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "OAMHUSyR#hPismBN4CGe5n_uFF6GRytdqc6mou7t-fF5ephIqKDE",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://pomf2.lain.la/f/uzu4feg.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    // Session ID: Put your session ID here if config.env is not working
+    SESSION_ID: process.env.SESSION_ID || "OAMHUSyR#hPismBN4CGe5n_uFF6GRytdqc6mou7t-fF5ephIqKDE",
+
+    // Alive Image URL
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/uzu4feg.jpg",
+
+    // Alive Message
+    ALIVE_MSG: process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
+
+    // Auto Read Status (true/false)
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
